6ad82b0edbd9a459dc878bb6cb80a7b357996fcc
diptarup saha <diptarup.saha.5@hotmail.com>
2024-06-02 08:16:20 +0530

    Section 2 -First End Point Completed.

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,25 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas() // returns a list
+        {
+            return VillaStore.villaList;
+            
+        }
+        [HttpGet("id")]
+        public VillaDTO GetVilla(int id) // returns a single data
+        {
+            return VillaStore.villaList.FirstOrDefault( u =>u.Id==id);
+            
+        }
+    }
+}
